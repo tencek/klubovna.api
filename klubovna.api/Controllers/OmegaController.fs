17721dd0c79ec3ae9df648e02c16ea259d63a9a1
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-03-07 00:58:47 +0100

    + OmegaController

---
+++ klubovna.api/Controllers/OmegaController.fs
@@ -0,0 +1,11 @@
+﻿namespace klubovna.api.Controllers
+
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+type OmegaController () =
+    inherit Controller()
+
+    [<HttpPost("heartbeat")>]
+    member this.Post([<FromBody>] token:string) =
+        new OkResult()
