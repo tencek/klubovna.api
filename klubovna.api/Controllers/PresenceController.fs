459a1ef5d96595777a08844f3007e6f87478dc8e
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-03-07 00:21:41 +0100

    + PresenceController

---
+++ klubovna.api/Controllers/PresenceController.fs
@@ -0,0 +1,11 @@
+﻿namespace klubovna.api.Controllers
+
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+type PresenceController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Get() =
+        false
