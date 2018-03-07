69651aa4cc9b84d48340c74f326ef901d4461ff9
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-03-07 01:26:30 +0100

    heartbeat chnage

--- klubovna.api/Controllers/OmegaController.fs
+++ klubovna.api/Controllers/OmegaController.fs
@@ -1,11 +1,16 @@
 ﻿namespace klubovna.api.Controllers
 
 open Microsoft.AspNetCore.Mvc
+open System.Net
 
 [<Route("api/[controller]")>]
 type OmegaController () =
     inherit Controller()
 
     [<HttpPost("heartbeat")>]
-    member this.Post([<FromBody>] token:string) =
-        new OkResult()
+    [<ProducesResponseType(typeof<unit>, 200)>]
+    [<ProducesResponseType(401)>]
+    member this.Post([<FromBody>] token:string) : IActionResult =
+        match token with
+        | "12345" -> new OkResult() :> IActionResult
+        | _ -> new UnauthorizedResult() :> IActionResult
